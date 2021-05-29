221a672f40696ca3dd3611387f62f4cb7f40f353
Eduardo Gonik <gonik@quimica.unlp.edu.ar>
2021-05-29 17:43:11 -0300

    trato de que sea lo mas portable posible

---
+++ path_scripts/add_to_path.sh
@@ -0,0 +1,9 @@
+echo  "export PATH=$PWD:$PATH" >> ~/.zshrc
+source ~/.zshrc
+print Ultimas lineas de ~/.zshrc =
+tail ~/.zshrc -n 5
+print variable PATH=
+echo $PATH
+pip install pygithub gitpython python-dotenv
+chmod 760 downloader_gv
+cat secrets.env_file > dotenv_prueba
